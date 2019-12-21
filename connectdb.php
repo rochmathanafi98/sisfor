711a8814b47967cd283c127140f4ff37edc29acf
rochmat_hanafii <rochmathanafi20@gmail.com>
2019-12-21 09:18:12 +0700

    Update connectdb.php

--- connectdb.php
+++ connectdb.php
@@ -4,8 +4,8 @@
 
   $db_username = "root";
   $db_hostname = "localhost";
-  $db_password = "Upeje2013";
-  $db_name = "sisforn";
+  $db_password = "";
+  $db_name = "sisfo4";
 
   $con = _connect($db_hostname, $db_username, $db_password);
   $db  = _select_db($db_name, $con);
